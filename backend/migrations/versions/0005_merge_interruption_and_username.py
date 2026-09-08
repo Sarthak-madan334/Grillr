9427d04f0bc517a3b061ab78a10b5af813304f1b
Sarthak <sarthakmadan88@gmail.com>
2026-09-08 15:36:01 +0530

    fix: shorten Alembic merge revision id

--- backend/migrations/versions/0005_merge_interruption_and_username.py
+++ backend/migrations/versions/0005_merge_interruption_and_username.py
@@ -1,12 +1,12 @@
 """merge interruption state and username migration branches
 
-Revision ID: 0005_merge_interruption_and_username
+Revision ID: 0005_merge_user_interrupt
 Revises: 0003_add_interruption_state, 0004_add_answer_idempotency_key
 """
 from typing import Sequence, Union
 
 
-revision: str = "0005_merge_interruption_and_username"
+revision: str = "0005_merge_user_interrupt"
 down_revision: Union[str, Sequence[str], None] = (
     "0003_add_interruption_state",
     "0004_add_answer_idempotency_key",
